c90bc862223936d9169821efaaab56e62e400829
Virtual Sense <virtual_sense@naver.com>
2021-05-18 16:21:09 +0900

    Create GenericcDelegate.cs

---
+++ GenericcDelegate.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericcDelegate
+{
+    delegate int Compare<T>(T a, T b);
+
+    class MainApp
+    {
+        static int AscendCompare<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b);
+        }
+        static void BubbleSort<T>(T[] DataSet, Compare<T> Comparer)
+        {
+            int i = 0;
+            int j = 0;
+            T temp;
+
+            for(i=0; i<DataSet.Length-1; i++)
+            {
+                for(j=0; j<DataSet.Length - (i+1); j++)
+                {
+                    if (Comparer(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 2, 10};
+
+            Console.WriteLine("Sorting ascending...");
+            BubbleSort<int>(array, new Compare<int>(AscendCompare));
+
+            for (int i = 0; i < array.Length; i++)
+                Console.Write($"{array[i]} ");
+        }
+    }
+}
