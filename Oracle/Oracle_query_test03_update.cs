40efa00bfdc588a48f2e577b9e34cbce89d13b0b
Virtual Sense <virtual_sense@naver.com>
2021-05-21 13:32:53 +0900

    Create Oracle_query_test03_update.cs

---
+++ Oracle/Oracle_query_test03_update.cs
@@ -0,0 +1,39 @@
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oracle_query_test03_update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConn = "Data Source=(DESCRIPTION=" +
+                "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+                "(HOST=localhost)(PORT=1521)))" +
+                "(CONNECT_DATA=(SERVER=DEDICATED)" +
+                "(SERVICE_NAME=xe)));" +
+                "User Id=hr;Password=hr;";
+
+            //1.연결 객체 만들기 - Client
+            OracleConnection conn = new OracleConnection(strConn);
+            //2.데이터베이스 접속을 위한 연결
+            conn.Open();
+
+            //3.서버와 함께 신나게 놀기
+            // ~~~~~~~~
+            //3.1 Query 명령객체 만들기
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn; //연결객체와 연동
+            //3.5업데이트
+            cmd.CommandText = "UPDATE PhoneBook SET HP='010-7777-77777' " +
+                              "WHERE ID = 2";
+            cmd.ExecuteNonQuery();
+            //4. 리소스 반환 및 종료
+            conn.Close();
+        }
+    }
+}
