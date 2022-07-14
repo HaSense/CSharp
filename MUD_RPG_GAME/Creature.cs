b5d45a96d435940b1c627f97a0b896ca268af8f8
Virtual Sense <virtual_sense@naver.com>
2022-07-14 14:58:24 +0900

    Update Creature.cs

--- MUD_RPG_GAME/Creature.cs
+++ MUD_RPG_GAME/Creature.cs
@@ -1,20 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MUD_RPG_GAME
+public enum CreatureType
 {
-    internal class Program
+    None,
+    Player = 1,
+    Monster = 2
+}
+class Creature
+{
+    CreatureType type;
+    protected Creature(CreatureType type)
     {
-        static void Main(string[] args)
-        {
-            Game game = new Game();
-            while (true)
-            {
-                game.Process();
-            }
-        }
+        this.type = type;
+    }
+    protected int hp = 0;
+    protected int attack = 0;
+
+    public void SetInfo(int hp, int attack)
+    {
+        this.hp = hp;
+        this.attack = attack;
+    }
+
+    public int GetHp() { return hp; }
+    public int GetAttack() { return attack; }
+
+    public bool isDead() { return hp <= 0; }
+    public void OnDamaged(int damage)
+    {
+        hp -= damage;
+        if (hp < 0)
+            hp = 0;
     }
 }
