7bf1100bdfda5b0ed4b6d6ee9efe18926fa89708
Virtual Sense <virtual_sense@naver.com>
2022-07-18 10:48:12 +0900

    1 - 100까지 함수로 분리연습

---
+++ StructuredCode/002.LoopTest.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Core_exam001
+{
+    class Calculrator
+    {
+        public int InputValue()
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            return N;
+        }
+        public int Logic(int N)
+        {
+            int sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 입력을 받은 정수까지 합을 프로그램을 작성하시오.
+            // N? 100
+            // 5050
+            // Calculrator의 기능 메소드로 만들어 주세요.
+            // 1. 입력 (변수입력)
+            // 2. 로직 --> Loop 메소드
+            // 3. 출력 --> 메소드. 
+
+            Calculrator calculrator = new Calculrator();
+
+            int N = calculrator.InputValue();
+            int sum = 0;
+            
+            sum = calculrator.Logic(N);
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
