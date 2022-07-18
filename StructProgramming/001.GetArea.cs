272f027a3ff4ce61149af7ce3c49b8216dc29934
Virtual Sense <virtual_sense@naver.com>
2022-07-18 10:10:43 +0900

    Update 001.GetArea.cs

--- StructProgramming/001.GetArea.cs
+++ StructProgramming/001.GetArea.cs
@@ -22,26 +22,31 @@ namespace DotNet_Core_001
         }
         static int InputWidth()
         {
-            int width = Int32.Parse(Console.ReadLine());
-            return width;
+            return Int32.Parse(Console.ReadLine());
             
         }
         static int InputHeight()
         {
-            int height = Int32.Parse(Console.ReadLine());
-            return height;
+            return Int32.Parse(Console.ReadLine());
+        }
+        static void InputVariablet(ref int w, ref int h)
+        {
+            w = Int32.Parse(Console.ReadLine());
+            h = Int32.Parse(Console.ReadLine());
         }
         static void Main(string[] args)
         {
             //1. 변수 선언 , 
-            int width;
-            int height;
+            int width = 0;
+            int height = 0;
             int area;
             //2.입력받기
             //width = Int32.Parse(Console.ReadLine());
             //height = Int32.Parse(Console.ReadLine());
-            width = InputWidth();
-            height = InputHeight();
+            //width = InputWidth();
+            //height = InputHeight();
+            InputVariablet(ref width, ref height);
+
             //3. 로직
             area = (width * height);
             //4. 출력
