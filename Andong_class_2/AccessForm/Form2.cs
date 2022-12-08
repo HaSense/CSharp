45eb7f2286ce0ca7408ad12cb8f7b0c11e12a93e
Virtual Sense <virtual_sense@naver.com>
2022-12-08 13:40:21 +0900

    Create Form2.cs

---
+++ Andong_class_2/AccessForm/Form2.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsAccessTest
+{
+    
+    public partial class Form2 : Form
+    {
+        Form1 frm1;     // HAS-A ((포함))
+        public Form2() //디폴트 생성자
+        {
+            InitializeComponent();
+        }
+        public Form2(object form)       //생성자를 하나 더 만듦
+        {
+            InitializeComponent();
+
+            frm1 = (Form1)form;
+        }
+        public Form2(string str, Form1 form1)
+        {
+            this.frm1 = form1;
+            str = "Hello";
+        }
+
+        private void btnChangeMainLabel_Click(object sender, EventArgs e)
+        {
+            frm1.lblForm1.Text = "Form2에서 변경을 시켰습니다.";
+            
+        }
+    }
+}
