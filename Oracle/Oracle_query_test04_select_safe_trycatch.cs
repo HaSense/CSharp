b05b7c0d3564d0ecfe7982322680f9f398ea70b1
Virtual Sense <virtual_sense@naver.com>
2023-07-17 11:00:57 +0900

    Create Oracle_query_test04_select_safe_trycatch.cs

---
+++ Oracle/Oracle_query_test04_select_safe_trycatch.cs
@@ -0,0 +1,59 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string strConn = "Data Source=(DESCRIPTION=" +
+            "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+            "(HOST=localhost)(PORT=1521)))" +
+            "(CONNECT_DATA=(SERVER=DEDICATED)" +
+            "(SERVICE_NAME=xe)));" +
+            "User Id=hr;Password=hr;";
+
+        OracleConnection conn = null;
+        OracleCommand cmd = null;
+        OracleDataReader rdr = null;
+
+        try
+        {
+            conn = new OracleConnection(strConn);
+            conn.Open();
+
+            cmd = new OracleCommand("SELECT * FROM PhoneBook", conn);
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                if (!rdr.IsDBNull(rdr.GetOrdinal("ID")))
+                {
+                    int id = rdr.GetInt32(rdr.GetOrdinal("ID"));
+                    string name = rdr.IsDBNull(rdr.GetOrdinal("NAME")) ? null : rdr.GetString(rdr.GetOrdinal("NAME"));
+                    string hp = rdr.IsDBNull(rdr.GetOrdinal("HP")) ? null : rdr.GetString(rdr.GetOrdinal("HP"));
+
+                    Console.WriteLine($"{id} : {name} : {hp}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            if (rdr != null)
+            {
+                rdr.Dispose();
+            }
+
+            if (cmd != null)
+            {
+                cmd.Dispose();
+            }
+
+            if (conn != null)
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+        }
+    }
+}
