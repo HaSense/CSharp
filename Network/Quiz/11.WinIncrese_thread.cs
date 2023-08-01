5bf3c1a85ba88c9a5f096e1715386de5da33b531
Virtual Sense <virtual_sense@naver.com>
2023-08-01 16:25:09 +0900

    Update 11.WinIncrese_thread.cs

--- Network/Quiz/11.WinIncrese_thread.cs
+++ Network/Quiz/11.WinIncrese_thread.cs
@@ -1,1 +1,51 @@
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinIncreseApp02
+{
+    public partial class Form1 : Form
+    {
+        private int cnt = 0;
+        private Thread t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            t = new Thread(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Invoke((Action)(() => label1.Text = cnt++ + ""));
+                        Thread.Sleep(1000);
+                    }catch (Exception ex)
+                    {
+                        break; 
+                    }
+                    
+                }
+            });
+            t.Start();
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Interrupt();
+        }
+    }
+}
