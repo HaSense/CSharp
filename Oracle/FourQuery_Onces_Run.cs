72690c32e2ef5dd847f478a90e842a8aa13f1787
Virtual Sense <virtual_sense@naver.com>
2023-08-10 13:49:31 +0900

    한번에 실행

--- Oracle/FourQuery_Onces_Run.cs
+++ Oracle/FourQuery_Onces_Run.cs
@@ -1,36 +1,60 @@
-/*
-  현재 코드는 함수만 참고하세요.
-  결과적으로 ExecuteNonQuery()로 한번에 실행하면 됩니다.
-/*
-using (OracleConnection conn = new OracleConnection(connectionString))
+/* 
+  한번에 다수의 쿼리를 사용하려면 
+  P/L SQL 문법을 사용하여 처리할 수 있습니다.
+*/
+
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace ThreeQueryOnce
 {
-    conn.Open();
-    OracleCommand cmd = conn.CreateCommand();
-    
-    // 트랜잭션 시작
-    OracleTransaction transaction = conn.BeginTransaction(IsolationLevel.ReadCommitted);
-    cmd.Transaction = transaction;
-
-    try
+    internal class Program
     {
-        // 4개의 쿼리를 연결
-        string query1 = "UPDATE table1 SET column1 = value1 WHERE condition1;";
-        string query2 = "UPDATE table2 SET column2 = value2 WHERE condition2;";
-        string query3 = "UPDATE table3 SET column3 = value3 WHERE condition3;";
-        string query4 = "UPDATE table4 SET column4 = value4 WHERE condition4;";
+        static void Main(string[] args)
+        {
+            //SELECT 데이터 조회
+            string strConn = "Data Source=(DESCRIPTION=" +
+                "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+                "(HOST=localhost)(PORT=1521)))" +
+                "(CONNECT_DATA=(SERVER=DEDICATED)" +
+                "(SERVICE_NAME=xe)));" +
+                "User Id=hr;Password=hr;";
 
-        string combinedQuery = query1 + query2 + query3 + query4;
+            OracleConnection conn = new OracleConnection(strConn);
 
-        cmd.CommandText = combinedQuery;
-        cmd.ExecuteNonQuery();
 
-        // 모든 쿼리가 성공하면 트랜잭션 커밋
-        transaction.Commit();
-    }
-    catch (Exception ex)
-    {
-        // 에러가 발생하면 롤백
-        transaction.Rollback();
-        Console.WriteLine(ex.ToString());
+            conn.Open();
+            OracleCommand cmd = conn.CreateCommand();
+
+            // 트랜잭션 시작
+            OracleTransaction transaction = conn.BeginTransaction(IsolationLevel.ReadCommitted);
+            cmd.Transaction = transaction;
+
+            try
+            {
+                string combinedQuery = @"
+                     BEGIN
+                            UPDATE DEPT2 SET LOC='서울' WHERE LOC='BOSTON';
+                            UPDATE DEPT2 SET LOC='울산' WHERE LOC='CHICAGO';
+                            UPDATE DEPT2 SET LOC='부산' WHERE LOC='NEW YORK';
+                            UPDATE DEPT2 SET LOC='안동' WHERE LOC='DALLAS';
+                     END;";
+
+                cmd.CommandText = combinedQuery;
+                cmd.ExecuteNonQuery();
+
+                //모든 쿼리가 성공하면 트랜잭션 커밋
+                transaction.Commit();
+                Console.WriteLine("동작성공");
+            }
+            catch (Exception ex)
+            {
+                // 에러가 발생하면 롤백
+                transaction.Rollback();
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("동작실패");
+            }
+
+        }
     }
 }
