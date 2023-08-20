16645cdeff14f3b671f6fafa9e4b0025ef2725b2
Virtual Sense <virtual_sense@naver.com>
2023-08-21 06:39:55 +0900

    Update HomeController.cs

--- Web/Core/App02/Controllers/HomeController.cs
+++ Web/Core/App02/Controllers/HomeController.cs
@@ -2,11 +2,36 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RoutingWithoutMVC.Controllers
 {
+    //[Route("Home")]
+    [Route("[controller]")]
     public class HomeController : Controller
     {
+        [Route("")]
+        //[Route("Index")]
+        [Route("[action]")]
+        [Route("~/")]
         public IActionResult Index()
         {
             return View();
         }
+
+        //[Route("About")]
+        [Route("[action]")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        //[Route("Details/{id?}")]
+        [Route("[action]/{id?}")]
+        public int Details(int? id)
+        {
+            return id ?? 1;
+        }
+
+        //public ViewResult Details(int id)
+        //{
+        //    return View();
+        //}
     }
 }
