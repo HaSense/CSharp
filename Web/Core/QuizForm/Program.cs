66e7df9e0acf76e1fa155805bb6b5a02599c44f4
Virtual Sense <virtual_sense@naver.com>
2023-08-22 13:57:14 +0900

    Create Program.cs

---
+++ Web/Core/QuizForm/Program.cs
@@ -0,0 +1,16 @@
+ builder.Services.AddControllersWithViews();
+ var app = builder.Build();
+
+ app.UseStaticFiles();
+ app.UseRouting();
+ //app.MapControllerRoute(
+ //    name: "default",
+ //    pattern: "{controller=Home}/{action=Index}/{id?}"
+ //);
+ app.UseEndpoints(endpoints =>
+ {
+     endpoints.MapControllerRoute(
+         name: "default",
+         pattern: "{controller=Home}/{action=Index}/{id?}");
+ });
+ app.Run();
