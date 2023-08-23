f0ad52910b039db2da43cf200f1b85407a01ed37
Virtual Sense <virtual_sense@naver.com>
2023-08-23 11:02:23 +0900

    Create StudentModel.cs

---
+++ Web/Core/09.Model_MVC/Model/StudentModel.cs
@@ -0,0 +1,10 @@
+namespace WebMVC_Model1.Models
+{
+    public class StudentModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string HP { get; set; }
+        public string Major { get; set; }
+    }
+}
