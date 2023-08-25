54ad39f0339e81c6c58d6a087b27fab6b327badb
Virtual Sense <virtual_sense@naver.com>
2023-08-25 10:28:29 +0900

    Model

---
+++ Web/Core/10.StronglyTypedView/Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace StronglyTypedViewTest1.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Designation { get; set; } 
+        public int Salary { get; set; }
+
+    }
+}
