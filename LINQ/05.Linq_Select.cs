460ecb88a1ba2d9b50fa6b3f3f8efe2eb2648ec3
Virtual Sense <virtual_sense@naver.com>
2023-08-25 11:13:16 +0900

    검색 Select

---
+++ LINQ/05.Linq_Select.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Linq_Select
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public int AGE { get; set; }
+        public string JOB { get; set; }
+    }
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseOracle("User Id=c##scott;Password=tiger;Data Source=127.0.0.1/XE;");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new PersonContext())
+            {
+                var allPersons = context.Person.ToList();
+
+                // 헤더 출력
+                Console.WriteLine($"{nameof(Person.ID),-5} {nameof(Person.NAME),-15} {nameof(Person.AGE),-5} {nameof(Person.JOB),-15}");
+                Console.WriteLine(new string('-', 40));
+
+                // 데이터 출력
+                foreach (var person in allPersons)
+                {
+                    Console.WriteLine($"{person.ID,-5} {person.NAME,-15} {person.AGE,-5} {person.JOB,-15}");
+                }
+            }
+        }
+    }
+}
