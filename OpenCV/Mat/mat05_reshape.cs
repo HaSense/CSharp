470aba43aa28ccd6d3dd29226cccd9c0db688624
Virtual Sense <virtual_sense@naver.com>
2023-10-18 11:32:09 +0900

    Create mat05_reshape.cs

---
+++ OpenCV/Mat/mat05_reshape.cs
@@ -0,0 +1,38 @@
+using OpenCvSharp;
+using System;
+
+namespace OpenCvSharpSample
+{
+    class CVInfo
+    {
+        public void PrintMatInfo(string m_name, Mat m)
+        {
+            Console.WriteLine($"[{m_name} 행렬]");
+            Console.WriteLine($"   채널 수 = {m.Channels()}");
+            Console.WriteLine($"   행 X 열 = {m.Rows} x {m.Cols}\n");
+            Console.WriteLine($"행렬 :\n{m.Dump()}\n");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mat m1 = new Mat(2, 6, MatType.CV_8U);
+            Mat m2 = m1.Reshape(2, m1.Rows * m1.Cols);
+            Mat m3 = m1.Reshape(3, 2);
+
+            CVInfo cvinfo = new CVInfo();
+            cvinfo.PrintMatInfo("m1(2, 6)", m1);
+            cvinfo.PrintMatInfo("m2 = m1_reshape(2)", m2);
+            cvinfo.PrintMatInfo("m3 = m1_reshape(3, 2)", m3);
+
+            m1.Create(3, 5, MatType.CV_16S);
+            cvinfo.PrintMatInfo("m1.create(3, 5)", m1);
+
+            m1.Dispose();
+            m2.Dispose();
+            m3.Dispose();
+        }
+    }
+}
