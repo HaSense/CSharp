039c6003251443dc0139874ef51b73a08519b3ed
Virtual Sense <virtual_sense@naver.com>
2023-10-18 14:38:35 +0900

    Update cvKeyboard.cs

--- OpenCV/KeyMouse/cvKeyboard.cs
+++ OpenCV/KeyMouse/cvKeyboard.cs
@@ -1,20 +1,25 @@
 using OpenCvSharp;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace OpenCvSharpSample
+namespace cvKeyboardTest
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             using (Mat image = new Mat(200, 300, MatType.CV_8U, new Scalar(255)))
             {
-                Cv2.NamedWindow("키보드 이벤트", WindowMode.AutoSize);
+                Cv2.NamedWindow("키보드 이벤트", WindowFlags.AutoSize);
                 Cv2.ImShow("키보드 이벤트", image);
 
                 while (true)
                 {
-                    int key = Cv2.WaitKey(200);
+                    int key = Cv2.WaitKeyEx(200);
+                    
                     if (key == 27) // 'ESC' key
                         break;
 
@@ -32,20 +37,21 @@ namespace OpenCvSharpSample
                         case (int)'B':
                             Console.WriteLine("B키 입력");
                             break;
-                        case 81:
+                        case 2424832: // 왼쪽 화살표
                             Console.WriteLine("왼쪽 화살표 키 입력");
                             break;
-                        case 82:
+                        case 2490368: // 위쪽 화살표
                             Console.WriteLine("위쪽 화살표 키 입력");
                             break;
-                        case 83:
+                        case 2555904: // 오른쪽 화살표
                             Console.WriteLine("오른쪽 화살표 키 입력");
                             break;
-                        case 84:
+                        case 2621440: // 아래쪽 화살표
                             Console.WriteLine("아래쪽 화살표 키 입력");
                             break;
-                        // default:
-                        //     Console.WriteLine(key);
+                            //default:
+                            //    Console.WriteLine(key);
+                            //    break;
                     }
                 }
             }
