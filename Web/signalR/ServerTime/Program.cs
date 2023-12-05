588e7e251769f4936114d1d69b1401072af484e5
Virtual Sense <virtual_sense@naver.com>
2023-12-05 09:57:38 +0900

    Create Program.cs

---
+++ Web/signalR/ServerTime/Program.cs
@@ -0,0 +1,45 @@
+using SignalRApp01.Hubs;
+using System.Threading;
+
+namespace SignalRApp01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            // SignalR 서비스 추가
+            builder.Services.AddSignalR();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            //서버스 경로추가
+            app.MapHub<TimeHub>("/timeHub"); // SignalR 허브 경로 설정
+
+            app.Run();
+        }
+    }
+}
