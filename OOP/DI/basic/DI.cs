1cfb33f043c715eefe654184529be62d400bdeda
Virtual Sense <virtual_sense@naver.com>
2023-12-14 14:42:40 +0900

    Update DI.cs

--- OOP/DI/basic/DI.cs
+++ OOP/DI/basic/DI.cs
@@ -17,6 +17,15 @@ namespace DICode
             return "엔진이 시작됩니다.";
         }
     }
+    /*
+      이런 형태로 만들면 IEngine 인터페이스를 구현하는 다른 클래스들도 Car 클래스에
+      쉽게 주입할 수 있습니다.
+      
+      Engine클래스가 변경되어도 Car 클래스는 항상 고정됩니다.
+
+      예) DieselEngine, ElectricEngine, LPIEngine
+    */
+    
     public class Car
     {
         private IEngine _engine;
