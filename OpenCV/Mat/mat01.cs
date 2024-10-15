a10604285f10b0cd8153991c5884cbb641d224d2
Virtual Sense <virtual_sense@naver.com>
2024-10-15 14:06:40 +0900

    mat01.cs

--- OpenCV/Mat/mat01.cs
+++ OpenCV/Mat/mat01.cs
@@ -1,39 +1,39 @@
 using OpenCvSharp;
-using System;
+using System.Runtime.InteropServices;
 
-namespace OpenCvSharpSample
+namespace OpenCV_dotnet6
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            float[] data = {
-                1.2f, 2.3f, 3.2f,
-                4.5f, 5.0f, 6.5f
+            double[] data =
+            {
+                1.2, 2.3, 3.2,
+                4.5, 5.0, 6.5
             };
 
-            Mat m1 = new Mat(2, 3, MatType.CV_8U);
-            Mat m2 = new Mat(2, 3, MatType.CV_8U, new Scalar(300));
+            Mat m1 = new Mat(2, 3, MatType.CV_8U, new Scalar(0));
+            Mat m2 = new Mat(2, 3, MatType.CV_8U, new Scalar(255));
             Mat m3 = new Mat(2, 3, MatType.CV_16S, new Scalar(300));
-            Mat m4 = new Mat(2, 3, MatType.CV_32F, data);
+            Mat m4 = new Mat(2, 3, MatType.CV_32F);
+            Marshal.Copy(data, 0, m4.Data, data.Length); ;
 
-            Size sz = new Size(3, 2);
-            Mat m5 = new Mat(sz, MatType.CV_64F);
-            Mat m6 = new Mat(sz, MatType.CV_32F, data);
+            Size sz = new Size(2, 3);
+            Mat m5 = new Mat(sz, MatType.CV_64F, new Scalar(0));
 
             Console.WriteLine("m1 : \n" + m1.Dump());
             Console.WriteLine("m2 : \n" + m2.Dump());
             Console.WriteLine("m3 : \n" + m3.Dump());
-            Console.WriteLine("m4 : \n" + m4.Dump());
+            Console.WriteLine("m4 : \n" + m3.Dump());
             Console.WriteLine("m5 : \n" + m5.Dump());
-            Console.WriteLine("m6 : \n" + m6.Dump());
 
             m1.Dispose();
             m2.Dispose();
             m3.Dispose();
             m4.Dispose();
             m5.Dispose();
-            m6.Dispose();
+
         }
     }
 }
