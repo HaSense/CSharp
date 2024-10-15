44f5340137f71cc196c75cc33e133110aef276b9
Virtual Sense <virtual_sense@naver.com>
2024-10-15 16:27:59 +0900

    Update mat05_reshape.cs

--- OpenCV/Mat/mat05_reshape.cs
+++ OpenCV/Mat/mat05_reshape.cs
@@ -27,8 +27,8 @@ namespace OpenCvSharpSample
             cvinfo.PrintMatInfo("m2 = m1_reshape(2)", m2);
             cvinfo.PrintMatInfo("m3 = m1_reshape(3, 2)", m3);
 
-            m1.Create(3, 5, MatType.CV_16S);
-            cvinfo.PrintMatInfo("m1.create(3, 5)", m1);
+            //m1.Create(3, 5, MatType.CV_16S);
+            //cvinfo.PrintMatInfo("m1.create(3, 5)", m1);
 
             m1.Dispose();
             m2.Dispose();
