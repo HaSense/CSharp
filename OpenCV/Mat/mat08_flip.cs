02543728e027c0fefb0ae8b94c66c03d435768c4
Virtual Sense <virtual_sense@naver.com>
2024-10-15 15:38:30 +0900

    FLIP

--- OpenCV/Mat/mat08_flip.cs
+++ OpenCV/Mat/mat08_flip.cs
@@ -1,20 +1,16 @@
 using OpenCvSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Flip_CV
+namespace MatFlip
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Mat image = new Mat("flip.jpg", ImreadModes.Color);
+            String path = @"C:/Temp/opencv/flip_test.jpg";
+            Mat image = new Mat(path, ImreadModes.Color);
             if (image.Empty())
             {
-                throw new System.Exception("이미지가 로드되지 않았습니다.");
+                Console.WriteLine("경로나 이미지에 문제가 있습니다.");
             }
 
             Mat x_axis = new Mat();
@@ -22,23 +18,24 @@ namespace Flip_CV
             Mat xy_axis = new Mat();
             Mat rep_img = new Mat();
             Mat trans_img = new Mat();
-
+            
             Cv2.Flip(image, x_axis, FlipMode.X);
             Cv2.Flip(image, y_axis, FlipMode.Y);
             Cv2.Flip(image, xy_axis, FlipMode.XY);
 
-            Cv2.Repeat(image, 1, 2, rep_img);
+            Cv2.Repeat(image, 2, 2, rep_img);
             Cv2.Transpose(image, trans_img);
 
-            new Window("image", image);
-            new Window("x_axis", x_axis);
-            new Window("y_axis", y_axis);
-            new Window("xy_axis", xy_axis);
-            new Window("rep_img", rep_img);
-            new Window("trans_img", trans_img);
-            Cv2.WaitKey();
-            
+            Cv2.ImShow("image", image);
+            Cv2.ImShow("x_axis", x_axis);
+            Cv2.ImShow("y_axis", y_axis);
+            Cv2.ImShow("xy_axis", xy_axis);
+            Cv2.ImShow("rep_img", rep_img);
+            Cv2.ImShow("trans_img", trans_img);
+
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+
         }
     }
-    
 }
