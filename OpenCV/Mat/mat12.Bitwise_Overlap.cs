3fb6d3aed23ece720b89b1bc5fa884d3a564e74d
Virtual Sense <virtual_sense@naver.com>
2024-10-18 13:04:11 +0900

    Bitwise_Overlap
    OpenCV Sharp의  split가 4채널인지 3채널인지 확인하세요.

--- OpenCV/Mat/mat12.Bitwise_Overlap.cs
+++ OpenCV/Mat/mat12.Bitwise_Overlap.cs
@@ -1,9 +1,6 @@
 using OpenCvSharp;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp65
 {
@@ -33,23 +30,33 @@ namespace ConsoleApp65
             Cv2.Threshold(logo, logoTh, 70, 255, ThresholdTypes.Binary);
 
             // 로고 영상 채널 분리 (분리된 채널 수 확인)
-            masks = Cv2.Split(logoTh);
+            masks = Cv2.Split(logoTh); //알파채널이 분리가 안됨
 
             // 채널 수 확인
             if (masks.Length < 3)
             {
                 throw new Exception("로고 이미지는 최소한 3채널이어야 합니다.");
+                //확인해 보면 3개만 분리되고 mask[3]이 분리되어 나오지 않음!!!
+                //masks.Length를 4로 해보고나 masks.Length == 3으로 해보면 확인할 수 있음
             }
 
             // 전경 통과 마스크
             Mat mask1 = new Mat();
+            Cv2.ImShow("mask0", masks[0]);
+            Cv2.ImShow("mask1", masks[1]);
+            Cv2.ImShow("mask2", masks[2]);
+            //Cv2.ImShow("mask3", masks[3]); //분리되지 않으면 당연히 안나옵니다. 
+            
             Cv2.BitwiseOr(masks[0], masks[1], mask1);
             Mat mask3 = new Mat();
             Cv2.BitwiseOr(masks[2], mask1, mask3);
+            Cv2.ImShow("3개 mask합성", mask3);
 
             // 배경 통과 마스크
-            Mat maskBackground = new Mat();
-            Cv2.BitwiseNot(mask3, maskBackground);
+            Mat notMask = new Mat(mask1.Size(), MatType.CV_8UC1, new Scalar(255));
+            Cv2.BitwiseNot(mask3, notMask);
+            Cv2.ImShow("원본로고", logo);
+            Cv2.ImShow("bitwiseNot", notMask);
 
             // 영상 중심 좌표 계산
             Point center1 = new Point(image.Width / 2, image.Height / 2);  // 이미지 중심 좌표
@@ -63,7 +70,7 @@ namespace ConsoleApp65
             Mat background = new Mat();
             Mat foreground = new Mat();
             Cv2.BitwiseAnd(logo, logo, foreground, mask3);
-            Cv2.BitwiseAnd(new Mat(image, roi), new Mat(image, roi), background, maskBackground);
+            Cv2.BitwiseAnd(new Mat(image, roi), new Mat(image, roi), background, notMask);
 
             // 전경과 배경 합성
             Mat dst = new Mat();
