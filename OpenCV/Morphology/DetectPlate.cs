481ba2f65b9dcb805f1011610beae0d1d0dec156
Virtual Sense <virtual_sense@naver.com>
2024-10-25 10:49:09 +0900

    04.DetectPlate

--- OpenCV/Morphology/DetectPlate.cs
+++ OpenCV/Morphology/DetectPlate.cs
@@ -5,11 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DefectPlateApp
+namespace DefectPlate
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             while (true)
@@ -18,7 +17,7 @@ namespace DefectPlateApp
                 if (!int.TryParse(Console.ReadLine(), out int no) || no == 0)
                     break;
 
-                string fname = $"C:\\Temp\\img\\test_car\\{no:D2}.jpg"; //두 자리 숫자 01, 11, 12 등
+                string fname = $"C:/Temp/opencv/test_car/{no:D2}.jpg"; //두 자리로 꼭 사용하세요. 숫자 01, 11, 12 등
                 Mat image = new Mat(fname, ImreadModes.Color);
 
                 if (image.Empty())
@@ -27,28 +26,27 @@ namespace DefectPlateApp
                     continue;
                 }
 
-                // 이미지 처리
                 Mat gray = new Mat();
                 Mat sobel = new Mat();
                 Mat thImg = new Mat();
                 Mat morph = new Mat();
 
-                // 닫음 연산 마스크 생성
+                // 닫힘 연산 마스크 생성
                 Mat kernel = Mat.Ones(new Size(31, 5), MatType.CV_8UC1);
 
                 // 명암도 영상 변환
                 Cv2.CvtColor(image, gray, ColorConversionCodes.BGR2GRAY);
 
-                // 블러링
+                // 블러링을 통한 전처리
                 Cv2.Blur(gray, gray, new Size(5, 5));
 
-                // 소벨 에지 검출
+                // 소벨 알고리즘으로 경계(에지 검출한다)
                 Cv2.Sobel(gray, sobel, MatType.CV_8U, 1, 0, 3);
 
-                // 이진화
+                // 이진화를 수행한다.
                 Cv2.Threshold(sobel, thImg, 120, 255, ThresholdTypes.Binary);
 
-                // 닫음 연산
+                // 닫음 연산 - 팽창 후 침식을 통해 경계를 명확히 분리한다
                 Cv2.MorphologyEx(thImg, morph, MorphTypes.Close, kernel);
 
                 // 결과 표시
@@ -57,7 +55,6 @@ namespace DefectPlateApp
                 Cv2.ImShow("닫음 연산", morph);
                 Cv2.WaitKey(2000);
             }
-            
         }
     }
 }
