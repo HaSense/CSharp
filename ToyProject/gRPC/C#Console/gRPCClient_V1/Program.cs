57e2988b6bd0842bd5f1985cbb294f9ec4c6e0dc
Virtual Sense <virtual_sense@naver.com>
2024-11-22 09:33:01 +0900

    Console gRPC

---
+++ ToyProject/gRPC/C#Console/gRPCClient_V1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using gRPCServer_V1;
+
+namespace gPRCClient_V1
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            // HTTP/2 지원 채널 생성
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+
+            Console.WriteLine("[클라이언트] 서버에 메시지를 전송합니다...");
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "테스트 사용자" });
+            Console.WriteLine($"[클라이언트] 서버 응답: {reply.Message}");
+        }
+    }
+}
---
+++ ToyProject/gRPC/C#Console/gRPCServer_V1/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Builder;
+
+namespace gRPCServer_V1
+{
+    // gRPC 서비스 구현
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"[서버] 클라이언트로부터 받은 메시지: {request.Name}");
+            return Task.FromResult(new HelloReply
+            {
+                Message = $"안녕하세요, {request.Name}님!"
+            });
+        }
+    }
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        // HTTP/2 프로토콜 활성화
+                        options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
+                    });
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddGrpc();
+                    });
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapGrpcService<GreeterService>();
+                        });
+                    });
+                })
+                .Build();
+
+            Console.WriteLine("gRPC 서버가 실행 중입니다. 포트: 5000");
+            await host.RunAsync();
+        }
+    }
+
+}
